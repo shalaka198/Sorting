17eec9c4aec637e839bfe9c96dda7688c92cac3b
Shalaka Sakhardande <shalaka.sakhardande@carsales.com.au>
2019-06-28 16:35:31 +1000

    Implemented Merge sort and Quick sort

---
+++ MergeSort/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Sorting
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = {3, 10, 9, 15, 5, 7, 3, 8, 12};
+			MergeSort(arr);
+			foreach(var i in arr)
+			{
+				Console.WriteLine($"{i}");			
+			}
+		}
+		
+		//Divide and sort
+		static void MergeSort(int[] arr)
+		{
+			if(arr != null && arr.Length > 0)
+			{
+				var n = arr.Length;
+				if(n < 2)
+				{
+					return;
+				}
+				int mid = n/2;
+				int n_left = mid;
+				int n_right = n - n_left;
+				
+				int[] left_arr = new int[n_left];
+				for(var i=0; i<n_left; i++)
+				{
+					left_arr[i] = arr[i];
+				}	
+
+				int[] right_arr = new int[n_right];
+				for(int i=mid,j=0; i<n; i++,j++)
+				{
+					right_arr[j] = arr[i];
+				}	
+
+				MergeSort(left_arr);
+				MergeSort(right_arr);
+				Merge(left_arr, right_arr, arr);				
+			}
+		}
+
+		// Combine
+		static void Merge(int[] left_arr, int[] right_arr, int[] arr)
+		{
+			var n_left = left_arr.Length;
+			var n_right = right_arr.Length;
+
+			var i = 0;
+			var j = 0;
+			var k = 0;
+			
+			while(i<n_left && j<n_right)
+			{
+				if(left_arr[i] < right_arr[j])
+				{
+					arr[k] = left_arr[i];
+					i++;
+				}
+				else
+				{
+					arr[k] = right_arr[j];
+					j++;
+				}
+				k++;
+			}
+			
+			while(i<n_left){ arr[k] = left_arr[i]; i++; k++;}
+			
+			while(j<n_right){ arr[k] = right_arr[j]; j++; k++;}
+		}
+	}
+}
---
+++ QuickSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace QuickSort
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = {3,12, 10, 3, 6, 8, 7, 11};
+			QuickSort(arr, 0, arr.Length-1);
+			foreach(var i in arr)
+			{
+				Console.WriteLine($"{i}");
+			}
+
+		    Console.ReadKey();
+		}
+		
+		static void QuickSort(int[] arr, int start_index, int end_index)
+		{
+			// exit condition
+			if(start_index >= end_index)
+				return;						
+
+			var p_index = Partition(arr, start_index, end_index);
+			QuickSort(arr, start_index, p_index-1);
+			QuickSort(arr, p_index+1, end_index);
+		}
+
+		static int Partition(int[] arr, int start, int end)
+		{
+			int pivot = arr[end];
+			int p_index = start;
+			int temp = 0;
+			for(var i=start; i<=end-1; i++)
+			{
+				if(arr[i]<=pivot)
+				{
+					// swap element at ith position with element at pivot index; and increment pivot index by 1
+					temp = arr[i];
+					arr[i] = arr[p_index];
+					arr[p_index] = temp;
+					
+					p_index++; 
+				}				
+			}
+
+			// swap pivot and element at pivot index
+			arr[end] = arr[p_index];
+			arr[p_index] = pivot;
+
+			return p_index; 		
+		}
+	}
+}
